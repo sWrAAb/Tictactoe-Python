fb2d78d8eccc40c68935b733314214b53dde1e21
sWrAAb <swraab@gmail.com>
2021-03-31 18:44:17 +0100

    added random player start function

--- tictactoe.py
+++ tictactoe.py
@@ -6,15 +6,26 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 # Random player
 
-# random_player = 
+def random_player():
+    if random.randint(0,1) == 0:
+        print("Computer is first")
+        return "Computer"
+    else:
+        print("Player is first")
+        return "Player"
+
+random_player()
 
 # Display board
 
 def display_board():
+    print()
     print(" " + board[0] + " | " + board[1] + " | " + board[2])
     print("---+---+---")
     print(" " + board[3] + " | " + board[4] + " | " + board[5])
     print("---+---+---")
     print(" " + board[6] + " | " + board[7] + " | " + board[8])
+    print()
 
 display_board()
+
