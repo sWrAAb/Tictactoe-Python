b784fbdd33080106aeb04d5237f96687692a19be
sWrAAb <swraab@gmail.com>
2021-03-31 21:58:18 +0100

    added check if win function

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,18 @@ from os import system
 
 board = [" "," "," "," "," "," "," "," "," "]
 
+# Active game
+
+active_game = True
+
+# Winner
+
+winner = None
+
+# Current player
+
+current_player = "X"
+
 # Random player
 
 def random_player():
@@ -29,15 +41,27 @@ def display_board():
     print(" " + board[6] + " | " + board[7] + " | " + board[8])
     print()
 
-display_board()
+
 
 
 available_moves = ["1","2","3","4","5","6","7","8","9"]
 print("Available moves:")
 print(available_moves)
 
+def check_win():
+    if board[0] == board[1] == board[2] != " " or board[3] == board[4] == board[5] != " " or board[6] == board[7] == board[8] != " ":
+        print("Winner")
+    elif board[0] == board[3] == board[6] != " " or board[1] == board[4] == board[7] != " " or board[2] == board[5] == board[8] != " ":
+        print("Winner")
+    elif board[0] == board[4] == board[8] != " " or board[2] == board[4] == board[6] != " ":
+        print("Winner")
+
 def game():
-    game_turn()
+    display_board()
+    
+    while active_game:
+        game_turn()
+        check_win()
 
 def game_turn():
     position = input("Enter position: ")
@@ -47,3 +71,4 @@ def game_turn():
     display_board()
 
 game()
+
