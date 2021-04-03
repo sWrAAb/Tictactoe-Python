05b1b99c2f3e92143c40c6322356d242aef3e350
sWrAAb <swraab@gmail.com>
2021-04-03 14:35:57 +0100

    added switch player function and keys next to board, and fixed some bugs

--- tictactoe.py
+++ tictactoe.py
@@ -5,7 +5,7 @@ from os import system
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-# Active game
+# Active game. Game is going while this is true
 
 active_game = True
 count = 0
@@ -34,18 +34,18 @@ random_player()
 # Avaliable moves
 
 available_moves = ["1","2","3","4","5","6","7","8","9"]
-print("Available moves:")
+
 
 # Display board
 
 def display_board():
     '''Displays list as game board'''
     print()
-    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print(" " + board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
     print("---+---+---")
-    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print(" " + board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
     print("---+---+---")
-    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+    print(" " + board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
     print()
     print(available_moves)
 
@@ -55,6 +55,9 @@ def check_rows():
     row_1 = board[0] == board[1] == board[2] != " "
     row_2 = board[3] == board[4] == board[5] != " "
     row_3 = board[6] == board[7] == board[8] != " "
+    if row_1 or row_2 or row_3:
+        ''' If rows match game stops'''
+        active_game = False
     return
 
 def check_columns():
@@ -63,6 +66,9 @@ def check_columns():
     column_1 = board[0] == board[3] == board[6] != " "
     column_2 = board[1] == board[4] == board[7] != " "
     column_3 = board[2] == board[5] == board[8] != " "
+    if column_1 or column_2 or column_3:
+        ''' If columns match game stops'''
+        active_game = False
     return
 
 def check_diagonals():
@@ -70,6 +76,9 @@ def check_diagonals():
     ''' Checks if diagonals match'''
     diagonal_1 = board[0] == board[4] == board[8] != " "
     diagonal_2 = board[2] == board[4] == board[6] != " "
+    if diagonal_1 or diagonal_2:
+        ''' If diagonals match game stops'''
+        active_game = False
     return
 
 
@@ -81,9 +90,19 @@ def check_win():
 
 
 def check_tie():
+    ''' If all fields are full game stops'''
+    global count
+    global active_game
+    if count == 9:
+        active_game = False
     return
 
 def switch_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    elif current_player == "O":
+        current_player = "X"
     return
 
 def game():
@@ -91,7 +110,7 @@ def game():
     display_board()
     
     while active_game:
-        game_turn()
+        game_turn(current_player)
         check_win()
         check_tie()
         switch_player()
@@ -101,16 +120,21 @@ def game():
     elif winner == None:
         print("Tie!")
 
-def game_turn():
+def game_turn(player):
     global count
+    print(player + "'s turn.")
     position = input("Enter position: ")
+    '''fixes input so it matches board indexes'''
     position = int(position) -1
+    board[position] = player
     count += 1
+    print("Available moves:")
     print(available_moves)
-    board[position] = "X"
     display_board()
-    print(count)
+    print("Turn " + str(count))
     
 
 game()
+
+
 
