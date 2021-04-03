1f4a2a53f7ae39e71ca7b7624d072eab555bd308
sWrAAb <swraab@gmail.com>
2021-04-03 12:53:32 +0100

    check for win function broken into multiple functions, added turn counter

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 # Active game
 
 active_game = True
+count = 0
 
 # Winner
 
@@ -30,9 +31,15 @@ def random_player():
 
 random_player()
 
+# Avaliable moves
+
+available_moves = ["1","2","3","4","5","6","7","8","9"]
+print("Available moves:")
+
 # Display board
 
 def display_board():
+    '''Displays list as game board'''
     print()
     print(" " + board[0] + " | " + board[1] + " | " + board[2])
     print("---+---+---")
@@ -40,35 +47,70 @@ def display_board():
     print("---+---+---")
     print(" " + board[6] + " | " + board[7] + " | " + board[8])
     print()
+    print(available_moves)
 
+def check_rows():
+    ''' Checks if rows match'''
+    global active_game
+    row_1 = board[0] == board[1] == board[2] != " "
+    row_2 = board[3] == board[4] == board[5] != " "
+    row_3 = board[6] == board[7] == board[8] != " "
+    return
 
+def check_columns():
+    ''' Checks if columns match'''
+    global active_game
+    column_1 = board[0] == board[3] == board[6] != " "
+    column_2 = board[1] == board[4] == board[7] != " "
+    column_3 = board[2] == board[5] == board[8] != " "
+    return
 
+def check_diagonals():
+    global active_game
+    ''' Checks if diagonals match'''
+    diagonal_1 = board[0] == board[4] == board[8] != " "
+    diagonal_2 = board[2] == board[4] == board[6] != " "
+    return
 
-available_moves = ["1","2","3","4","5","6","7","8","9"]
-print("Available moves:")
-print(available_moves)
 
 def check_win():
-    if board[0] == board[1] == board[2] != " " or board[3] == board[4] == board[5] != " " or board[6] == board[7] == board[8] != " ":
-        print("Winner")
-    elif board[0] == board[3] == board[6] != " " or board[1] == board[4] == board[7] != " " or board[2] == board[5] == board[8] != " ":
-        print("Winner")
-    elif board[0] == board[4] == board[8] != " " or board[2] == board[4] == board[6] != " ":
-        print("Winner")
+    ''' Checks if board positions match '''
+    check_rows()
+    check_columns()
+    check_diagonals()
+
+
+def check_tie():
+    return
+
+def switch_player():
+    return
 
 def game():
+    '''Main function for gameplay'''
     display_board()
     
     while active_game:
         game_turn()
         check_win()
+        check_tie()
+        switch_player()
+
+    if winner == "X" or winner == "O":
+        print(winner + " won!")
+    elif winner == None:
+        print("Tie!")
 
 def game_turn():
+    global count
     position = input("Enter position: ")
     position = int(position) -1
-
+    count += 1
+    print(available_moves)
     board[position] = "X"
     display_board()
+    print(count)
+    
 
 game()
 
